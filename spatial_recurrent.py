21af56887d5474b7f9725a7f4d8970161e547497
Larry Neal <nealla@lwneal.com>
2017-04-06 17:21:13 -0700

    Actually use word input to determine a target

--- spatial_recurrent.py
+++ spatial_recurrent.py
@@ -1,3 +1,4 @@
+import random
 import os
 import sys
 import math
@@ -7,12 +8,18 @@ from PIL import Image
 import tensorflow as tf
 from keras import backend as K
 
+import words
 import imutil
-from cgru import SpatialCGRU, transpose, reverse
+from cgru import SpatialCGRU
+from words import MAX_WORDS
 
 
 # Output is RGB
+# NOTE: Input images must be square
 IMG_CHANNELS = 3
+
+cat = np.array(Image.open('kitten.jpg').resize((32,32)))
+dog = np.array(Image.open('puppy.jpg').resize((32,32)))
 
 
 def main(**params):
@@ -20,11 +27,16 @@ def main(**params):
     train(model, **params)
 
 
-def build_model(width, cgru_size_1, cgru_size_2, **params):
+def build_model(width, cgru_size_1, cgru_size_2, embed_size=256, **params):
     batch_size = params['batch_size']
 
-    # NOTE: All inputs are square
-    img = layers.Input(batch_shape=(batch_size, width, width, IMG_CHANNELS))
+    input_img = layers.Input(batch_shape=(batch_size, width, width, IMG_CHANNELS))
+
+    input_words = layers.Input(batch_shape=(batch_size, MAX_WORDS), dtype='int32')
+    
+    language = layers.Embedding(embed_size, words.VOCABULARY_SIZE)(input_words)
+    language = layers.GRU(embed_size)(language)
+    language = layers.Dense(embed_size)(language)
 
     # Apply the convolutional layers of VGG16
     from keras.applications.vgg16 import VGG16
@@ -33,7 +45,14 @@ def build_model(width, cgru_size_1, cgru_size_2, **params):
         layer.trainable = False
 
     # Run a pretrained network
-    x = vgg(img)
+    x = vgg(input_img)
+
+    # Broadcast language into every convolutional output
+    shape = map(int, x.shape)
+    language = layers.RepeatVector(shape[1] * shape[2])(language)
+    language = layers.Reshape((shape[1], shape[2], embed_size))(language)
+
+    x = layers.Concatenate()([x, language])
 
     # Statefully scan the image in each of four directions
     x = SpatialCGRU(x, cgru_size_1)
@@ -61,7 +80,7 @@ def build_model(width, cgru_size_1, cgru_size_2, **params):
     # Output an RGB image
     x = layers.Conv2D(3, (3,3), activation='sigmoid', padding='same')(x)
 
-    moo = models.Model(inputs=img, outputs=x)
+    moo = models.Model(inputs=[input_img, input_words], outputs=x)
     moo.compile(optimizer='adam', loss='mse')
     moo.summary()
     return moo
@@ -69,45 +88,47 @@ def build_model(width, cgru_size_1, cgru_size_2, **params):
 
 def train(model, model_filename, batches_per_epoch, **params):
     batch_size = params['batch_size']
-    X, Y = example(**params)
-    print("Input X:")
-    imutil.show(X)
-
-    print("Target Y:")
-    imutil.show(map_to_img(Y, **params))
 
     if model_filename and os.path.exists(model_filename):
         model.load_weights(model_filename)
 
     while True:
-        # Data for demo prediction
-        examples = [example(**params) for _ in range(batch_size)]
-        batch_X, batch_Y = map(np.array, zip(*examples))
-
-        # Predict
-        preds = model.predict(batch_X)[-1]
-        X = batch_X[-1]
-        Y = batch_Y[-1]
-        #print("Input:")
-        #imutil.show(X)
-        #print("Ground Truth:")
-        #imutil.show(map_to_img(Y, **params))
-        print("Network Output:")
-        imutil.show(X + map_to_img(preds, **params))
-
-        print("Training...")
-        # Train for a while
+        demo(model, **params)
         for i in range(batches_per_epoch):
-            examples = [example(**params) for _ in range(batch_size)]
-            batch_X, batch_Y = map(np.array, zip(*examples))
-            h = model.train_on_batch(np.array(batch_X), np.array(batch_Y))
-
+            batch_X, batch_Y = get_batch(**params)
+            model.train_on_batch(batch_X, batch_Y)
         if model_filename:
             model.save_weights(model_filename)
 
 
-cat = np.array(Image.open('kitten.jpg').resize((32,32)))
-dog = np.array(Image.open('puppy.jpg').resize((32,32)))
+def demo(model, **params):
+    batch_size = params['batch_size']
+    # Data for demo prediction
+    X, Y = get_batch(**params)
+    preds = model.predict(X)
+    print("Target (left) vs. Network Output (right):")
+    input_pixels, input_words = X[0][0], X[1][0]
+    print(words.words(input_words))
+    left = input_pixels + Y[0] * 255.
+    right = input_pixels + map_to_img(preds[0], **params)
+    imutil.show(np.concatenate((left, right), axis=1))
+
+
+def get_batch(**params):
+    width = params['width']
+    batch_size = params['batch_size']
+    batch_X_words = np.zeros((batch_size, MAX_WORDS), dtype=int)
+    batch_X_pixels = np.zeros((batch_size, width, width, IMG_CHANNELS))
+    batch_Y = np.zeros((batch_size, width, width, IMG_CHANNELS))
+
+    for i in range(batch_size):
+        input_pixels, input_words, target_pixels = example(**params)
+        batch_X_pixels[i] = input_pixels
+        batch_X_words[i] = input_words
+        batch_Y[i] = target_pixels
+
+    return [batch_X_pixels, batch_X_words], batch_Y
+
 
 def example(**params):
     width = params['width']
@@ -120,9 +141,18 @@ def example(**params):
 
     target = np.zeros((width, width, IMG_CHANNELS))
 
+    phrase = random.choice([
+        'draw a red cross through the cat',
+        'draw a blue circle around the dog',
+        'draw a green circle around the cat',
+    ])
+
     # Easy Target: A single layer CGRU gets this right away
     # Light up the row and column centered on the cat
-    #target += crosshair(cx, cy, color=0, **params)
+    if 'cross' in phrase:
+        x, y = (cx, cy) if 'cat' in phrase else (dx, dy)
+        color = 0 if 'red' in phrase else 1 if 'green' in phrase else 2
+        target += crosshair(x, y, color=0, **params)
 
     # Easy Target:
     # Light up a cone to the right of the cat
@@ -150,15 +180,16 @@ def example(**params):
     #target += circle(cx, cy, rad, color=0)
     #target += circle(dx, dy, rad, color=2)
 
-    # For fun, ALSO draw a blue circle around the cat
-    for r in range(30, 34):
-        target += circle(cx, cy, r, color=2, **params)
+    if 'circle' in phrase:
+        x, y = (cx, cy) if 'cat' in phrase else (dx, dy)
+        color = 0 if 'red' in phrase else 1 if 'green' in phrase else 2
+        target += circle(x, y, rmin=24, rmax=32, color=color, **params)
 
     target = smooth_gradient(target, **params)
-    return pixels, target
+    return pixels, words.indices(phrase), target
 
 
-def smooth_gradient(target, smoothing_epsilon=.05, max_filter_size=4, **params):
+def smooth_gradient(target, smoothing_epsilon=.05, max_filter_size=1, **params):
     # Blur to reward approximate correct answers
     if max_filter_size > 1:
         from scipy.ndimage.filters import maximum_filter
@@ -196,19 +227,21 @@ def right_cone(x, y, width, color=0, **params):
 def crosshair(x, y, width, color=0, **params):
     height = width
     Y = np.zeros((height, width, IMG_CHANNELS))
-    Y[y,:, color] = 1.0
-    Y[:,x, color] = 1.0
+    Y[y-4:y+4,:, color] = 1.0
+    Y[:,x-4:x+4, color] = 1.0
     return Y
 
 
-def circle(x, y, r, width, color=0, **params):
+def circle(x, y, rmin, rmax, width, color=0, **params):
     height = width
     Y = np.zeros((height, width, IMG_CHANNELS))
-    for t in range(628):
-        yi = y + r * math.cos(t / 100.)
-        xi = x + r * math.sin(t / 100.)
-        if 0 <= yi < height and 0 <= xi < width:
-            Y[int(yi), int(xi), color] = 1.0
+    resolution = 100.0
+    for t in range(int(3.14 * 2 * resolution)):
+        for r in range(rmin, rmax):
+            yi = y + r * math.cos(t / resolution)
+            xi = x + r * math.sin(t / resolution)
+            if 0 <= yi < height and 0 <= xi < width:
+                Y[int(yi), int(xi), color] = 1.0
     return Y
 
 
